bcbf3d71915f70e2a1ff3375f844f484e7ce849b
kirimine170 <kirimine170@gmail.com>
2023-12-15 14:30:59 +0900

    📝 add UIConfig.java
    ハードコーディング多すぎたのでとりまコンフィグ管理用のクラス実装。
    UIからコンフィグ編集できるようにしたい　

---
+++ app/src/main/java/pomodoro/ui/UIConfig.java
@@ -0,0 +1,16 @@
+package pomodoro.ui;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.stage.StageStyle;
+
+public class UIConfig
+{
+    public static final Color DEFAULT_ARC_COLOR = Color.PURPLE;
+    public static final Color DEFAULT_ELLIPSE_COLOR = Color.WHITE;
+    public static final Color DEFAULT_BACKGROUND_COLOR = Color.TRANSPARENT;
+
+    public static final ArcType DEFAULT_ARC_TYPE = ArcType.ROUND;
+
+    public static final StageStyle DEFAULT_STAGE_STYLE = StageStyle.TRANSPARENT;
+}
