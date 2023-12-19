f5727215976e10b4742e61ee5bb70ca8e840a1d1
kirimine170 <kirimine170@gmail.com>
2023-12-20 06:25:09 +0900

    ✏️ update PomodoroTImer.java
    セッション管理をどうにかした

--- app/src/main/java/pomodoro/timer/PomodoroTimer.java
+++ app/src/main/java/pomodoro/timer/PomodoroTimer.java
@@ -9,20 +9,91 @@ public class PomodoroTimer
 
     private long ONE_SECOND_IN_MILLIS = 1000;
     private long pomodoroTime = 25;//* 60;
-    private long shortBreakTime = 5 * 60;
-    private long longBreakTime = 30 * 60;
+    private long shortBreakTime = 5;// * 60;
+    private long longBreakTime = 30;// * 60;
     private int pomodorosUntilLongBreak = 4;
+    private int pomodorosCompleted = 0;
+    private boolean isTimerRunning = false;
 
-    private enum SessionType
+    public enum SessionType
     {
-        POMODORO, SHORT_BREAK, LONG_BREAK
+        POMODORO
+        {
+            @Override
+            public SessionType getNextSessionType(PomodoroTimer pomodoroTimer)
+            {
+                if (pomodoroTimer.pomodorosCompleted % pomodoroTimer.pomodorosUntilLongBreak == 0) return LONG_BREAK;
+                else return SHORT_BREAK;
+            }
+
+            @Override
+            public String getSessionName()
+            {
+                return "Pomodoro";
+            }
+
+            @Override
+            public double getSessionPeriod(PomodoroTimer pomodoroTimer)
+            {
+                return pomodoroTimer.pomodoroTime;
+            }
+        },
+        SHORT_BREAK
+        {
+            @Override
+            public SessionType getNextSessionType(PomodoroTimer pomodoroTimer)
+            {
+                return POMODORO;
+            }
+
+            @Override
+            public String getSessionName()
+            {
+                return "Short Break";
+            }
+
+            @Override
+            public double getSessionPeriod(PomodoroTimer pomodoroTimer)
+            {
+                return pomodoroTimer.shortBreakTime;
+            }
+        },
+        LONG_BREAK
+        {
+            @Override
+            public SessionType getNextSessionType(PomodoroTimer pomodoroTimer)
+            {
+                return POMODORO;
+            }
+
+            @Override
+            public String getSessionName()
+            {
+                return "Long Break";
+            }
+
+            @Override
+            public double getSessionPeriod(PomodoroTimer pomodoroTimer)
+            {
+                return pomodoroTimer.longBreakTime;
+            }
+        };
+
+        public abstract SessionType getNextSessionType(PomodoroTimer pomodoroTimer);
+
+        public abstract String getSessionName();
+
+        public abstract double getSessionPeriod(PomodoroTimer pomodoroTimer);
     }
+
+    private SessionType currentSessionType;
 
     private Timer timer;
 
     public PomodoroTimer()
     {
         this.timer = new Timer();
+        this.currentSessionType = SessionType.POMODORO;
     }
 
     public PomodoroTimer(long pomodoroTime, long shortBreakTime, long longBreakTime, int pomodorosUntilLongBreak)
@@ -46,11 +117,31 @@ public class PomodoroTimer
             {
                 timerListener.onTimerComplete();
             }
-        }, pomodoroTime * ONE_SECOND_IN_MILLIS);
+        }, (long) getCurrentSessionType().getSessionPeriod(this) * ONE_SECOND_IN_MILLIS);
+        isTimerRunning = true;
     }
 
     public void stopTimer()
     {
         this.timer.cancel();
+        isTimerRunning = false;
+    }
+
+    public SessionType getCurrentSessionType()
+    {
+        return this.currentSessionType;
+    }
+
+    public void nextSession()
+    {
+        this.timer = new Timer();
+        if (this.currentSessionType == SessionType.POMODORO) this.pomodorosCompleted++;
+        System.out.println(this.currentSessionType.getSessionName() + " " + this.pomodorosCompleted);
+        this.currentSessionType = this.currentSessionType.getNextSessionType(this);
+    }
+
+    public boolean isTimerRunning()
+    {
+        return this.isTimerRunning;
     }
 }
